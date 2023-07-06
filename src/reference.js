6ddfefc6ab6d928278dc029eae7cc8d6fcf279e8
Julien Veyssier <julien-nc@posteo.net>
2023-07-06 13:42:38 +0200

    remove nextcloud/vue-richtext, update npm pkgs
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/reference.js
+++ src/reference.js
@@ -19,7 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { registerWidget } from '@nextcloud/vue-richtext'
+import { registerWidget } from '@nextcloud/vue/dist/Components/NcRichText.js'
 import { linkTo } from '@nextcloud/router'
 import { getRequestToken } from '@nextcloud/auth'
 
