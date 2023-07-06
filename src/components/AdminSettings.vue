5eaeb1897c1d06bc1a329abdb2067f024cbc41e5
Julien Veyssier <julien-nc@posteo.net>
2023-07-06 13:46:20 +0200

    refs #8 fix mistake in a vue template
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/components/AdminSettings.vue
+++ src/components/AdminSettings.vue
@@ -12,7 +12,7 @@
 				</label>
 				<textarea id="peertube-instances"
 					v-model="state.instances"
-					:placeholder="…"
+					placeholder="…"
 					@input="onInput" />
 			</div>
 			<p class="settings-hint">
